601d1da18b0c45cbdaf496b991c766e3cce95aa0
ndfsa <sergiomarzana1@gmail.com>
2019-06-08 15:56:40 -0400

    Inicia screen v1.01

--- app/src/main/java/com/example/sarana/Inicia.java
+++ app/src/main/java/com/example/sarana/Inicia.java
@@ -8,21 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class Inicia extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicia);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
