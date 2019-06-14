fc9d9f138b2f61fa068b0e764604fab83d176d54
Andres <andresduarter13@gmail.com>
2019-06-14 15:58:56 -0400

    ultimas pantallas

---
+++ app/src/main/java/com/example/sarana/pasa_la_tarjeta.java
@@ -0,0 +1,13 @@
+package com.example.sarana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pasa_la_tarjeta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasa_la_tarjeta);
+    }
+}
